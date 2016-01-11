eeb9fe570f34d108cf83fc1bcdf7b34e77f39d62
Anna Becker and Sara Wagner <anna.r.becker@gmail.com>
2016-01-11 15:37:55 -0800

    Add flashcards.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+jQuery(function() {
+  jQuery("#javascript").click(function() {
+    $("#javascriptInfo").toggle();
+  });
+  jQuery("#modulo").click(function() {
+    $("#moduloInfo").toggle();
+  });
+  jQuery("#operators").click(function() {
+    $("#operatorsInfo").toggle();
+  });
+  jQuery("#NaN").click(function() {
+    $("#NaNinfo").toggle();
+  });
+  jQuery("#variable").click(function() {
+    $("#variableInfo").toggle();
+  });
+});
