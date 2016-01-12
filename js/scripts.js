0e9d9bf68ed41ba0b34722bdaecfdd358f6b4ad9
Anna Becker and Sara Wagner <anna.r.becker@gmail.com>
2016-01-11 16:21:06 -0800

    Finish flashcards.

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,43 @@ jQuery(function() {
   jQuery("#variable").click(function() {
     $("#variableInfo").toggle();
   });
+  jQuery("#plus").click(function() {
+    $("#plusInfo").toggle();
+  });
+  jQuery("#function").click(function() {
+    $("#functionInfo").toggle();
+  });
+  jQuery("#method").click(function() {
+    $("#methodInfo").toggle();
+  });
+  jQuery("#arguement").click(function() {
+    $("#arguementInfo").toggle();
+  });
+  jQuery("#string").click(function() {
+    $("#stringInfo").toggle();
+  });
+  jQuery("#chaining").click(function() {
+    $("#chainingInfo").toggle();
+  });
+  jQuery("#escape").click(function() {
+    $("#escapeInfo").toggle();
+  });
+  jQuery("#alert").click(function() {
+    $("#alertInfo").toggle();
+  });
+  jQuery("#commenting").click(function() {
+    $("#commentingInfo").toggle();
+  });
+  jQuery("#booleans").click(function() {
+    $("#booleansInfo").toggle();
+  });
+  jQuery("#undefined").click(function() {
+    $("#undefinedInfo").toggle();
+  });
+  jQuery("#parameter").click(function() {
+    $("#parameterInfo").toggle();
+  });
+  jQuery("#return").click(function() {
+    $("#returnInfo").toggle();
+  });
 });
